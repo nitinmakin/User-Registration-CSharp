358b0482018eb9a8a24786babc85a593e97b5230
nitinmakin <nitinmakin124.com>
2020-09-19 10:33:56 +0530

    Added-Welcome message

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello.. Welcome to User Validation Problem");
+        }
+    }
+}
