83e0c22501d6dacebd1da25eb234b693086322d9
nitinmakin <nitinmakin124.com>
2020-09-22 00:29:46 +0530

    Refactored code to decrease wornings

--- NUnitTestUserRegistration/UnitTest1.cs
+++ NUnitTestUserRegistration/UnitTest1.cs
@@ -7,7 +7,9 @@ namespace NUnitTestUserRegistration
     /// test class
     /// </summary>
     public class Tests
-    {
+    {/// <summary>
+    /// setup method
+    /// </summary>
         [SetUp]
         public void Setup()
         {
@@ -19,12 +21,27 @@ namespace NUnitTestUserRegistration
             bool result = userValidation.ValidateFirstName("Nitin");
             Assert.IsTrue(result);
         }
+
+        [Test]
+        public void GivenIncorrectFirstName_ShouldPassTest()
+        {
+            UserValidationMain userValidation = new UserValidationMain();
+            bool result = userValidation.ValidateFirstName("nitin");
+            Assert.IsFalse(result);
+        }
         [Test]
         public void GivenCorrectLastName_ShouldPassTest()
         {
             UserValidationMain userValidation = new UserValidationMain();
             bool result = userValidation.ValidateLastName("Makin");
             Assert.IsTrue(result);
+        }
+        [Test]
+        public void GivenIncorrectLastName_ShouldPassTest()
+        {
+            UserValidationMain userValidation = new UserValidationMain();
+            bool result = userValidation.ValidateLastName("makin");
+            Assert.IsFalse(result);
         }
         [Test]
         public void GivenCorrectEmail_ShouldPassTest()
@@ -34,11 +51,25 @@ namespace NUnitTestUserRegistration
             Assert.IsTrue(result);
         }
         [Test]
+        public void GivenIncorrectEmail_ShouldPassTest()
+        {
+            UserValidationMain userValidation = new UserValidationMain();
+            bool result = userValidation.ValidateEmail("nitin@makin124@gmail.com");
+            Assert.IsFalse(result);
+        }
+        [Test]
         public void GivenCorrectPhoneNo_ShouldPassTest()
         {
             UserValidationMain userValidation = new UserValidationMain();
             bool result = userValidation.ValidatePhoneNo("07829376277");
             Assert.IsTrue(result);
+        }
+        [Test]
+        public void GivenIncorrectPhoneNo_ShouldPassTest()
+        {
+            UserValidationMain userValidation = new UserValidationMain();
+            bool result = userValidation.ValidatePhoneNo("0-782937");
+            Assert.IsFalse(result);
         }
         [Test]
         public void GivenCorrectPAsswordRule1Min8Char_ShouldPassTest()
@@ -48,11 +79,25 @@ namespace NUnitTestUserRegistration
             Assert.IsTrue(result);
         }
         [Test]
+        public void GivenIncorrectPAsswordRule1Min8Char_ShouldPassTest()
+        {
+            UserValidationMain userValidation = new UserValidationMain();
+            bool result = userValidation.ValidatePasswordRule1("kin");
+            Assert.IsFalse(result);
+        }
+        [Test]
         public void GivenCorrectPAsswordRule2Alteast1Uppercase_ShouldPassTest()
         {
             UserValidationMain userValidation = new UserValidationMain();
             bool result = userValidation.ValidatePasswordRule2("NitinMakin");
             Assert.IsTrue(result);
+        }
+        [Test]
+        public void GivenIncorrectPAsswordRule2Alteast1Uppercase_ShouldPassTest()
+        {
+            UserValidationMain userValidation = new UserValidationMain();
+            bool result = userValidation.ValidatePasswordRule2("nekxmekiti");
+            Assert.IsFalse(result);
         }
         [Test]
         public void GivenCorrectPAsswordRule3Alteast1NumaricNumber_ShouldPassTest()
@@ -61,12 +106,25 @@ namespace NUnitTestUserRegistration
             bool result = userValidation.ValidatePasswordRule3("NitinMakin124");
             Assert.IsTrue(result);
         }
+        [Test]
+        public void GivenIncorrectPAsswordRule3Alteast1NumaricNumber_ShouldPassTest()
+        {
+            UserValidationMain userValidation = new UserValidationMain();
+            bool result = userValidation.ValidatePasswordRule3("NitinMakin");
+            Assert.IsFalse(result);
+        }
 
         [Test]
         public void GivenCorrectPAsswordRule4MustHaveOnly1SpecialChar_ShouldPassTest()
         {
             UserValidationMain userValidation = new UserValidationMain();
             Assert.IsTrue(userValidation.ValidatePasswordRule4("NitinMakin124@"));
+        }
+        [Test]
+        public void GivenIncorrectPAsswordRule4MustHaveOnly1SpecialChar_ShouldPassTest()
+        {
+            UserValidationMain userValidation = new UserValidationMain();
+            Assert.IsFalse(userValidation.ValidatePasswordRule4("NitinMakin124"));
         }
         [Test]
         public void TestCaseForValidatingAllTheEmails()
